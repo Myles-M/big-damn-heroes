9ececdfc7ac56b53caa2d981893cfa29eb05a35b
Myles <bdsc82@gmail.com>
2020-03-03 07:03:49 -0600

    Initial commit

---
+++ src/main/java/com/bigdamnheroes/bigdamnheroes/BigDamnHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.bigdamnheroes.bigdamnheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDamnHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDamnHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigdamnheroes/bigdamnheroes/BigDamnHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdamnheroes.bigdamnheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDamnHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
